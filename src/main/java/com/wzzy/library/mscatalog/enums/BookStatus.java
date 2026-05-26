6b73a67f9f1aa9ae52839520d66e60a8f86a0e9a
codewesleylima <gusterlima@hotmail.com>
2026-05-25 22:51:04 -0300

    chore: Remove all Java classes - keep folder structure and infrastructure

--- src/main/java/com/wzzy/library/mscatalog/enums/BookStatus.java
+++
@@ -1,9 +0,0 @@
-package com.wzzy.library.mscatalog.enums;
-
-public enum BookStatus {
-    AVAILABLE,
-    OUT_OF_STOCK,
-    DISCONTINUED,
-    ARCHIVED,
-    COMING_SOON
-}
--- src/main/java/com/wzzy/library/mscatalog/exception/InvalidISBNException.java
+++
@@ -1,9 +0,0 @@
-package com.wzzy.library.mscatalog.exception;
-
-import com.wzzy.library.bfflibrary.exception.ValidationException;
-
-public class InvalidISBNException extends ValidationException {
-    public InvalidISBNException(String isbn) {
-        super("Invalid ISBN format: " + isbn, "INVALID_ISBN");
-    }
-}
