bd15096771e2011896f0788f371a2636aaf99da6
codewesleylima <gusterlima@hotmail.com>
2026-06-03 04:17:20 -0300

    refactor(dto): change type id in classes DTO

--- src/main/java/com/wzzy/library/mscatalog/dto/AuthorDTO.java
+++ src/main/java/com/wzzy/library/mscatalog/dto/AuthorDTO.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthorDTO {
 
-    private Long id;
+    private UUID id;
     private String name;
     private String biography;
     private LocalDate birthDate;
--- src/main/java/com/wzzy/library/mscatalog/dto/BookDTO.java
+++ src/main/java/com/wzzy/library/mscatalog/dto/BookDTO.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookDTO {
 
-    private Long id;
+    private UUID id;
     private String title;
     private String isbn;
     private String description;
